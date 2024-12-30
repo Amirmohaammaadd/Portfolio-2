0ef548423081a89172dd3ed627f797b1ead66eb9
Amirmohammad <amirmuhaammaadd@gmail.com>
2024-12-30 20:00:39 +0330

    new texts

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,70 +3,74 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 3 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT =
+  "As a passionate and innovative Front-end Developer, I craft seamless, fine performance user experiences using cutting-edge technologies like JS, React.js, Next.js, and Typescript. My ability to transform complex requirements into intuitive, visually stunning web applications sets me apart. I’m driven by a relentless pursuit of excellence and a commitment to delivering clean, maintainable code. With a deep understanding of both design and functionality, I ensure every project I touch is not only user-friendly but also a work of art.";
 
 export const ABOUT_TEXT =
-  "I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 3 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.";
+  "I am a dedicated and versatile Front-end developer with a passion for creating efficient and user-friendly web applications. With 3 years of professional experience, I have worked with a variety of technologies, including React, Next.js, typescript, MySQL. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.";
 
 export const EXPERIENCES = [
   {
     year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    role: "Front-end Developer",
+    company: "VOSOUQ GOSTAR",
+    description:
+      "The VOSOUQ GOSTAR is a reputable firm specializing in providing comprehensive tax consulting and advisory services. The company offers a range of services, including tax planning, compliance, audits, and dispute resolution. With a team of skilled tax professionals, it helps businesses navigate the complex tax regulations, ensuring they remain compliant with national tax laws while optimizing their financial strategies. I also developed an admin panel for managing complaints, tickets, appointments, and viewing various reports. This system streamlines workflows, enhances user experience, and allows efficient tracking and resolution of issues. It offers a comprehensive interface for monitoring and managing requests, improving overall administrative productivity.",
+    technologies: ["Javascript", "React.js", "Next.js"],
   },
   {
     year: "2022 - 2023",
     role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    company: "KEYKAVOOS",
+    description:
+      "As a Front-End Developer at Keykavoos , I contributed to the development and execution of various client projects. My responsibilities included coding, and implementing user interfaces, ensuring they were responsive, user-friendly, and aligned with client specifications",
+    technologies: ["Javascript", "React.js", "Next.js"],
   },
   {
     year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    role: "Front-end Developer",
+    company: "freelance",
+    description:
+      "Experienced front-end developer specializing in building dynamic, responsive web applications using React and Next.js. Proficient in UI libraries like Ant Design (AntD), Material-UI (MUI), and Tailwind CSS to craft visually appealing and user-friendly interfaces. Skilled in managing state and custom functionalities, including advanced table interactions, file uploads, and persistent cart systems. Adept at enhancing user experiences with animations, navigation logic, and tailored solutions such as synchronized UI components. Strong focus on delivering efficient, scalable, and accessible solutions to meet diverse client needs.",
+    technologies: ["Javascript", "React.js", "Next.js"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "myevtsp.com",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "Developed a user-friendly tax payer website using React and Next.js, enabling seamless form submissions and real-time tax calculations. Designed with Ant Design for a professional UI and implemented secure data handling for user privacy.",
+    technologies: ["Antd", "tailwind", "React", "Next.js"],
   },
   {
-    title: "Task Management App",
+    title: "mpevtsp.com",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "Created the main panel for a taxpayer website using React and Ant Design, featuring a dashboard for tracking tax filings, payment history, and notifications. Integrated interactive charts and filters for data visualization and streamlined user access to essential tools and resources.",
+    technologies: ["Antd", "tailwind", "React", "Next.js"],
   },
   {
-    title: "Portfolio Website",
+    title: "Digi-shop",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+      "Developed a responsive shopping website with React and Next.js, featuring a user-friendly interface, persistent cart functionality, and seamless product browsing. Integrated Ant Design for design consistency and localStorage for data retention across sessions.",
+    technologies: ["Antd", "tailwind", "React", "Next.js"],
   },
   {
-    title: "Blogging Platform",
-    image: project4,
+    title: "Snapp-food website",
+    image: project1,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "Designed and developed a responsive food delivery website using React and Next.js, with real-time order tracking and intuitive navigation. Integrated Ant Design for a polished UI and implemented features for seamless menu browsing and secure checkout",
+    technologies: ["Javascript", "React.js", "Next.js"],
+  },
+  {
+    title: "Azki.com website",
+    image: project2,
+    description:
+      "Built a professional insurance website with React and Next.js, featuring policy comparison tools and a streamlined quote generation process. Integrated Ant Design for a modern interface and optimized the user journey for seamless navigation and accessibility.",
+    technologies: ["Js", "React", "antD"],
   },
 ];
 
