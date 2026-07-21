a4932fb9813cf15560e514c65be7f2b8dd1c4250
Amirmohammad Abdolahi <amirmohamad.abdolahi@zoodfood.com>
2026-07-21 12:43:19 +0330

    add yml file

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const repoName = "Portfolio-2";
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubActions ? `/${repoName}` : "",
+  assetPrefix: isGithubActions ? `/${repoName}/` : "",
+};
 
 export default nextConfig;
